ffb8e57e2646758eb85add3c165b52e4aeef434f
tkdwns3269 <tkdwns3269@naver.com>
2024-09-26 16:36:21 +0900

    20240926

--- 7_JSP/jspProject/src/main/java/com/kh/board/service/BoardService.java
+++ 7_JSP/jspProject/src/main/java/com/kh/board/service/BoardService.java
@@ -12,6 +12,7 @@ import com.kh.board.model.dao.BoardDao;
 import com.kh.board.model.vo.Attachment;
 import com.kh.board.model.vo.Board;
 import com.kh.board.model.vo.Category;
+import com.kh.board.model.vo.Reply;
 import com.kh.common.PageInfo;
 
 public class BoardService {
@@ -161,6 +162,30 @@ public class BoardService {
 		ArrayList<Attachment> list = new BoardDao().selectAttachmentList(conn, boardNo);
 		close(conn);
 		
+		return list;
+	}
+	
+	public int insertReply(Reply r) {
+		Connection conn = getConnection();
+		int result = new BoardDao().insertReply(conn, r);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+	
+	public ArrayList<Reply> selectReplyList(int boardNo){
+		Connection conn = getConnection();
+		
+		ArrayList<Reply> list = new BoardDao().selectReplyList(conn, boardNo);
+		
+		close(conn);
 		return list;
 	}
 }
